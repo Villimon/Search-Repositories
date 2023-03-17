c623337da02e183e7b44e57d4f88b86c6cffd738
Your Name <you@example.com>
2023-03-17 15:32:07 +0300

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,73 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+const form = document.getElementById('form')
+const repositories = document.querySelector('.repositories')
 
-const swiper = new Swiper();
 
-console.log('hello');
+form.addEventListener('submit', async function (e) {
+    e.preventDefault()
+
+    const formData = new FormData(form)
+    let repo = formData.get('repo');
+
+    let result;
+
+    while (repositories.firstChild) {
+        repositories.removeChild(repositories.firstChild);
+    }
+
+
+    try {
+        let response = await fetch(`https://api.github.com/search/repositories?q=${repo}&per_page=10`, {
+            headers: {
+                'accept': 'application/vnd.github+json',
+                'Content-Type': 'application/json',
+            }
+        });
+        result = await response.json()
+    } catch (error) {
+        alert(error);
+    }
+
+
+    if (result) {
+        result.items.map(item =>
+            repositories.insertAdjacentHTML(
+                'beforeend',
+                `
+                <div class="repositories__body">
+                    <a href=${item.html_url} target="_blank" class="repositories__name">${item.name}</a>
+                    <div class="repositories__owner">
+                        <img src=${item.owner.avatar_url} alt="" class="repositories__img">
+                        <h3 class="repositories__login">${item.owner.login}</h3>
+                    </div>
+                </div>
+                `
+            )
+        )
+    }
+    if (result.total_count === 0) {
+        repositories.insertAdjacentHTML(
+            'beforeend',
+            `
+            <div class="repositories__body">
+                <h2 class="repositories__title">Ничего не найдено</h2>
+            </div>
+            `
+        )
+    }
+
+
+    form.reset()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
